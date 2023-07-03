57c2584394b7d3410efdfb3affa07ba2d5b09f8b
stasika <anasean999@gmail.com>
2023-07-03 04:38:17 +0300

    task 4 done

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,32 @@
+
 function Student(name, gender, age) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
   
-}
-
-Student.prototype.setSubject = function (subjectName) {
+    this.setSubject = function(subjectName) {
+      this.subject = subjectName;
+    };
   
-}
-
-Student.prototype.addMarks = function (...marks) {
+    this.addMarks = function(...marksToAdd) {
+      if (!this.marks) {
+        this.marks = [];
+      }
+      this.marks.push(...marksToAdd);
+    };
   
-}
-
-Student.prototype.getAverage = function () {
+    this.getAverage = function() {
+      if (!this.marks || this.marks.length === 0) {
+        return 0;
+      }
+      const sum = this.marks.reduce((total, mark) => total + mark, 0);
+      return sum / this.marks.length;
+    };
   
-}
-
-Student.prototype.exclude = function (reason) {
-  
-}
+    this.exclude = function(reason) {
+      delete this.subject;
+      delete this.marks;
+      this.excluded = reason;
+    };
+  }
